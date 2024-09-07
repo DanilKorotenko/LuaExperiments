d43bc8ffd809af6fc0111e97ed8858f9020bc5c1
Danil Korotenko <danil.korotenko@gmail.com>
2024-09-07 17:22:46 +0300

    First commit.

---
+++ FirstScript.lua
@@ -0,0 +1,7 @@
+
+player1 = { position = {x=1, y=3} }
+player2 = { position = {x=2, y=2} }
+player3 = { position = {x=4, y=1} }
+player4 = { position = {x=6, y=5} }
+
+wePlayer = { position = {x=3, y=6} }
